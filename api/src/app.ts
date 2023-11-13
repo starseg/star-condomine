b3e3115372683dd556d2c55b658754a063a7f5a1
Micael Miranda <micaelmiranda124@gmail.com>
2023-11-13 18:07:40 -0300

    Atualização no app

--- api/src/app.ts
+++ api/src/app.ts
@@ -29,33 +29,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 3000;
-
 app.get("/", (request, response) => {
   response.json({ message: "API DO SISTEMA STAR CONDOMINE" });
 });
 
-// MANIPULAÇÃO DE OPERADORES DO SISTEMA (OPERATORS)
+// === MANIPULAÇÃO DE OPERADORES (OPERATORS) === \\
 app.get("/operator", getAllOperators);
 app.get("/operators/:id", getOperator);
 app.post("/operators", createOperator);
 app.put("/operators/:id", updateOperator);
 app.delete("/operators/:id", deleteOperator);
 
-// MANIPULAÇÃO DE MEMBROS (MEMBERS)
+// === MANIPULAÇÃO DE MEMBROS (MEMBERS) === \\
 app.get("/members", getAllMembers);
 app.get("/members/:id", getMember);
 app.post("/members", createMember);
 app.put("/members/:id", updateMember);
 app.delete("/members/:id", deleteMember);
 
-// MANIPULAÇÃO DE PORTARIAS (LOBBIES)
+// === MANIPULAÇÃO DE PORTARIAS (LOBBIES) === \\
 app.get("/lobbies", getAllLobbies);
 app.get("/lobbies/:id", getLobby);
 app.post("/lobbies", createLobby);
 app.put("/lobbies/:id", updateLobby);
 app.delete("/lobbies/:id", deleteLobby);
 
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`✨ Servidor rodando na porta ${port} ✨`);
 });
