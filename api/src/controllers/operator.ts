3ae44d42b5fb3516eda639a5a81ad50d576ecadf
Micael Miranda <micaelmiranda124@gmail.com>
2023-11-14 18:14:07 -0300

    Adição do typescript no bcrypt

--- api/src/controllers/operator.ts
+++ api/src/controllers/operator.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
