04bd59a6c7398ac396f9374de0022da7d0556243
Micael Miranda <micaelmiranda124@gmail.com>
2023-11-17 17:56:21 -0300

    Atualizações básicas na página inicial

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,28 +1,19 @@
 "use client";
-import { Label } from "@/components/ui/label";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
- 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
+import Image from "next/image";
 import { InputForm } from "@/components/loginForm";
 
 export default function Home() {
   return (
     <main className="dark flex min-h-screen flex-col items-center justify-center p-24 bg-stone-950">
-      <h1 className="text-stone-50 text-4xl pb-8">Entrar na plataforma</h1>
-      <InputForm/>
+      <Image
+        src="/logo.png"
+        alt="Logo Star Condomine"
+        width={400}
+        height={100}
+        className="pb-8"
+      />
+      {/* <h1 className="text-stone-50 text-4xl pb-8">Entrar na plataforma</h1> */}
+      <InputForm />
     </main>
   );
 }
