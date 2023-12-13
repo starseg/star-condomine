31aa312ee745e84be7d2455e078538b21a18b1a2
Micael Miranda <micaelmiranda124@gmail.com>
2023-12-13 11:07:45 -0300

    Ajuste no logging

--- api/src/middlewares/logging.ts
+++ api/src/middlewares/logging.ts
@@ -9,12 +9,11 @@ export const logging = async (
   // Obtém informações relevantes da requisição
   try {
     const { method, url, operator } = req;
-    const userAgent = req.get("User-Agent") || "Desconhecido";
     const operatorId = parseInt(operator.operatorId, 10) || 0;
 
     // Registra as informações no banco de dados
     // await createLogging(method, url, userAgent, operatorId);
-    console.log(method, url, userAgent, operatorId);
+    console.log(method, url, operatorId);
     // Continue para a próxima middleware ou rota
     next();
   } catch (error) {
