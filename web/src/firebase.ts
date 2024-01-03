9b3bae2971180b4b9ae895ce7eed6017c0fbf24c
Micael Miranda <micaelmiranda124@gmail.com>
2024-01-03 11:57:48 -0300

    Firebase bucket update

--- web/src/firebase.ts
+++ web/src/firebase.ts
@@ -4,12 +4,12 @@
 
 export const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "star-condomine.firebaseapp.com",
-  projectId: "star-condomine",
-  storageBucket: "star-condomine.appspot.com",
+  authDomain: "starseg-7a8f4.firebaseapp.com",
+  projectId: "starseg-7a8f4",
+  storageBucket: "starseg-7a8f4.appspot.com",
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // // Initialize Firebase
