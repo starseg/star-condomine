4da75aba0209daf546bb45f87bbe09ac00c2e46a
Micael Miranda <micaelmiranda124@gmail.com>
2024-03-19 09:39:13 -0300

    correção nas migrations do prisma

--- api/prisma/migrations/20240206130216_cascade_deletions/migration.sql
+++ api/prisma/migrations/20240206130216_cascade_deletions/migration.sql
@@ -1,11 +1,11 @@
 -- DropForeignKey
-ALTER TABLE `tag` DROP FOREIGN KEY `Tag_memberId_fkey`;
+ALTER TABLE `Tag` DROP FOREIGN KEY `Tag_memberId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `telephone` DROP FOREIGN KEY `Telephone_memberId_fkey`;
+ALTER TABLE `Telephone` DROP FOREIGN KEY `Telephone_memberId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `vehicle` DROP FOREIGN KEY `Vehicle_memberId_fkey`;
+ALTER TABLE `Vehicle` DROP FOREIGN KEY `Vehicle_memberId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `Telephone` ADD CONSTRAINT `Telephone_memberId_fkey` FOREIGN KEY (`memberId`) REFERENCES `Member`(`memberId`) ON DELETE CASCADE ON UPDATE CASCADE;
