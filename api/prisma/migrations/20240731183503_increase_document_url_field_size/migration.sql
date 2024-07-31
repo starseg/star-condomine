2d7e7227c0758926564efe1ebf6cfc1a66f5508c
Micael Miranda <micaelmiranda124@gmail.com>
2024-07-31 15:35:37 -0300

    fix database field length error

---
+++ api/prisma/migrations/20240731183503_increase_document_url_field_size/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE `member` MODIFY `profileUrl` VARCHAR(255) NULL,
+    MODIFY `documentUrl` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `visitor` MODIFY `profileUrl` VARCHAR(255) NULL,
+    MODIFY `documentUrl` VARCHAR(255) NULL;
